965c4ed81026b2f13cfefda7e45dadef0354ad3d
Abduraimov <abduraim0vnurtilek26@gmail.com>
2024-01-26 00:11:50 +0600

    Initial Commit

---
+++ 4MonthHomeWork4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  4MonthHomeWork4
+//
+//  Created by Nurtilek on 1/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
